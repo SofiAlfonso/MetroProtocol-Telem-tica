0afec8ca6ab75f2a9725e3fb90f88291ca5efbe1
ansofalfons_meli <anasofia.alfonso@mercadolibre.com.co>
2025-09-30 15:54:19 -0500

    fix: linux log file update

--- Application/Server/utils.c
+++ Application/Server/utils.c
@@ -1,19 +1,17 @@
-//
-// Created by Ana Sofia Alfonso Moncada on 21/09/25.
-//
-
+// utils.c
 #include <stdio.h>
 #include <stdarg.h>
-#include <signal.h>
 #include <pthread.h>
-#include <sys/types.h>
+#include <time.h>
 
 FILE *logfile = NULL;
 pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void log_msg(const char *fmt, ...) {
-    va_list ap;
+    va_list ap, ap2;
     va_start(ap, fmt);
+    va_copy(ap2, ap); // copiar antes de usar ap
+
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
     char timestr[64];
@@ -21,20 +19,21 @@ void log_msg(const char *fmt, ...) {
 
     pthread_mutex_lock(&log_mutex);
 
+    // imprimir en stdout
     printf("[%s] ", timestr);
     vprintf(fmt, ap);
     printf("\n");
 
+    // imprimir en logfile si existe
     if (logfile) {
-        va_list ap2;
-        va_copy(ap2, ap);
         fprintf(logfile, "[%s] ", timestr);
         vfprintf(logfile, fmt, ap2);
         fprintf(logfile, "\n");
         fflush(logfile);
-        va_end(ap2);
     }
+
     pthread_mutex_unlock(&log_mutex);
 
+    va_end(ap2);
     va_end(ap);
 }
